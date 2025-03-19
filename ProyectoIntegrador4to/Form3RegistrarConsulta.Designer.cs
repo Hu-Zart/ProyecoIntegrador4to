40ba864a061327ddccfaae0235038eb8dcb7798b
BrauCam <brauliocamargo34@gmail.com>
2025-03-19 12:30:24 -0600

    Agrege btn de prueba para reisrar consuta 19/03 12:27PM

--- ProyectoIntegrador4to/Form3RegistrarConsulta.Designer.cs
+++ ProyectoIntegrador4to/Form3RegistrarConsulta.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbPrueba = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbPrueba
+            // 
+            this.lbPrueba.AutoSize = true;
+            this.lbPrueba.Location = new System.Drawing.Point(341, 311);
+            this.lbPrueba.Name = "lbPrueba";
+            this.lbPrueba.Size = new System.Drawing.Size(60, 20);
+            this.lbPrueba.TabIndex = 0;
+            this.lbPrueba.Text = "Prueba";
+            // 
+            // Form3RegistrarConsulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbPrueba);
+            this.Name = "Form3RegistrarConsulta";
             this.Text = "Form3RegistrarConsulta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbPrueba;
     }
 }
