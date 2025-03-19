63a94806af9730842c86d377a0ff2eb261447781
BrauCam <brauliocamargo34@gmail.com>
2025-03-19 12:35:23 -0600

    Deje en limpio el from3 19/03 12:34

--- ProyectoIntegrador4to/Form3RegistrarConsulta.Designer.cs
+++ ProyectoIntegrador4to/Form3RegistrarConsulta.Designer.cs
@@ -29,52 +29,27 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.lbPrueba = new System.Windows.Forms.Label();
-            this.tbPrueba = new System.Windows.Forms.TextBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
-            // lbPrueba
-            // 
-            this.lbPrueba.AutoSize = true;
-            this.lbPrueba.Location = new System.Drawing.Point(227, 202);
-            this.lbPrueba.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lbPrueba.Name = "lbPrueba";
-            this.lbPrueba.Size = new System.Drawing.Size(41, 13);
-            this.lbPrueba.TabIndex = 0;
-            this.lbPrueba.Text = "Prueba";
-            // 
-            // tbPrueba
-            // 
-            this.tbPrueba.Location = new System.Drawing.Point(230, 218);
-            this.tbPrueba.Name = "tbPrueba";
-            this.tbPrueba.Size = new System.Drawing.Size(100, 20);
-            this.tbPrueba.TabIndex = 1;
-            // 
             // contextMenuStrip1
             // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // Form3RegistrarConsulta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
-            this.Controls.Add(this.tbPrueba);
-            this.Controls.Add(this.lbPrueba);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(800, 449);
             this.Name = "Form3RegistrarConsulta";
             this.Text = "Form3RegistrarConsulta";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lbPrueba;
-        private System.Windows.Forms.TextBox tbPrueba;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
